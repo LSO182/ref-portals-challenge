e9aa53de239f522463510aeb844be1b7ca19b10e
sanchezolmedoluciano@gmail.com <sanchezolmedoluciano@gmail.com>
2025-05-14 10:55:44 -0300

    Feat: adding portals on ResultModal component

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,12 @@
-import { useRef, useImperativeHandle  } from "react";
+import { useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
-export default function ResultModal({ ref, timeRemaining ,targetTime, onReset }) {
+export default function ResultModal({
+  ref,
+  timeRemaining,
+  targetTime,
+  onReset,
+}) {
   const dialog = useRef();
   const userLost = timeRemaining <= 0;
   const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2);
@@ -14,19 +20,25 @@ export default function ResultModal({ ref, timeRemaining ,targetTime, onReset })
     };
   });
 
-  return (
+  return createPortal(
     <dialog className="result-modal" ref={dialog}>
-     {userLost && <h2>You losse</h2>}
-     {!userLost && <h2>Your score: <strong>{score}</strong> </h2>}
+      {userLost && <h2>You losse</h2>}
+      {!userLost && (
+        <h2>
+          Your score: <strong>{score}</strong>{" "}
+        </h2>
+      )}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopeed the timer with {formattedTimeRemaining} <strong>seconds left.</strong>
+        You stopeed the timer with {formattedTimeRemaining}{" "}
+        <strong>seconds left.</strong>
       </p>
       <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 }
